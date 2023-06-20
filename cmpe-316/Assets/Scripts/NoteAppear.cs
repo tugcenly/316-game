9cd463320681d1de62d759469876dc5d0207a8e7
tugcenly <96438350+tugcenly@users.noreply.github.com>
2023-06-20 21:59:53 +0300

    Not Ekleme
    Her odaya 1'er tane olmak üzere toplamda 4 tane not eklendi.

---
+++ cmpe-316/Assets/Scripts/NoteAppear.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NoteAppear : MonoBehaviour
+{
+    [SerializeField]
+    private Image noteImage;
+  
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")){
+            noteImage.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")){
+            noteImage.enabled = false;
+        }
+    }
+}
