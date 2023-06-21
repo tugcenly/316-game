5e3e6b0e47c9d1c01f35d151b11318fb4361fe6b
tugcenly <96438350+tugcenly@users.noreply.github.com>
2023-06-21 21:41:59 +0300

    note system
    note system tamamlandı, sadece hikaye ekleme kaldı.

--- cmpe-316/Assets/Scripts/NoteAppear.cs
+++ cmpe-316/Assets/Scripts/NoteAppear.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class NoteAppear : MonoBehaviour
 {
     [SerializeField]
     private Image noteImage;
-  
+    public Text noteText ;
+    
+    void Start()
+    {
+        noteText.enabled = false;
+
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")){
             noteImage.enabled = true;
+            noteText.enabled = true;
+
         }
     }
 
@@ -19,6 +29,7 @@ public class NoteAppear : MonoBehaviour
     {
         if (other.CompareTag("Player")){
             noteImage.enabled = false;
+            noteText.enabled = false;
         }
     }
 }
